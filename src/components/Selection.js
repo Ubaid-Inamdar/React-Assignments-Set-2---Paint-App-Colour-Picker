306f7bd2a10969912b646c0b30fd854c20a0ec04
Ubaid Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-12-06 00:10:37 +0530

    color picker1

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,13 @@
+import React, { useState } from "react";
+import "../styles/Child.css";
 
+function Selection({ applyColor }) {
+  let [color, setColor] = useState({ background: "" });
+  return (
+    <div className="fix-box" style={color} onClick={() => applyColor(setColor)}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+}
+
+export default Selection;
